87c16fef1ac118fe0a6d10df0a0cc0d9e81fb963
DilyanSvetiev <dsvetiev@gmail.com>
2024-08-22 21:37:33 +0300

    add: chess-board component with style and required imports for initial display of the chess board

---
+++ src/app/modules/chess-board/chess-board.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import { ChessBoard } from '../../chess-logic/chess-board';
+import { Color, FENChar } from '../../chess-logic/models';
+
+@Component({
+  selector: 'app-chess-board',
+  standalone: true,
+  imports: [CommonModule,ChessBoardComponent],
+  templateUrl: './chess-board.component.html',
+  styleUrl: './chess-board.component.css' 
+})
+export class ChessBoardComponent {
+  private chessBoard = new ChessBoard();
+  public chessBoardView: (FENChar | null)[][] = this.chessBoard.chessBoardView;
+  public get playerColor(): Color { return this.chessBoard.playerColor; } 
+}

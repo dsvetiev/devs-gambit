7396762b5cb68d3be4f355bd2fa3063188e50985
DilyanSvetiev <dsvetiev@gmail.com>
2024-08-24 19:39:51 +0300

    add: getter of safeSquares method in ChessBoardComponent

--- src/app/modules/chess-board/chess-board.component.ts
+++ src/app/modules/chess-board/chess-board.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChessBoard } from '../../chess-logic/chess-board';
-import { Color, Coords, FENChar, pieceImagePaths } from '../../chess-logic/models';
+import { Color, Coords, FENChar, pieceImagePaths, SafeSquares } from '../../chess-logic/models';
 import { SelectedSquare } from './models';
 
 @Component({
@@ -19,16 +19,23 @@ export class ChessBoardComponent {
   private selectedSquare: SelectedSquare = { piece: null };
   private pieceSafeSquares: Coords[] = [];
 
-  public get playerColor(): Color { return this.chessBoard.playerColor; } 
+  public get playerColor(): Color { 
+    return this.chessBoard.playerColor; 
+  };
 
   public isSquareDark(x: number, y: number): boolean {
     return ChessBoard.isSquareDark(x, y);
-  }
+  };
 
   public selectingPiece(x: number, y: number): void {
     const piece: FENChar | null = this.chessBoardView[x][y];
     if(!piece) return;
 
     this.selectedSquare = { piece, x, y};
-  }
+    this.pieceSafeSquares = this.safeSquares.get(x + "," + y) || [];
+  };
+
+  public get safeSquares(): SafeSquares {
+    return this.chessBoard.safeSquares;
+  };
 }

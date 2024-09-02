b2d5aaba9140abc38b362c5097a894b12317f8c9
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-02 19:47:49 +0300

    add: stockfish best move method and method required for converting the request and response in stockfish API required format

--- src/app/modules/computer-mode/stockfish.service.ts
+++ src/app/modules/computer-mode/stockfish.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
+import { ChessMove, StockfishQueryParams, StockFishResponse } from './models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockfishService {
+  private readonly api: string = "https://stockfish.online/api/s/v2.php";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  private convertColumnLetterToYCoord(string: string): number {
+    return string.charCodeAt(0) - 'a'.charCodeAt(0);
+  }
+
+  private moveFromStockfishString(move: string): ChessMove {
+    const prevY: number = this.convertColumnLetterToYCoord(move[0]);
+    const prevX: number = Number(move[1]) - 1;
+    const newY: number = this.convertColumnLetterToYCoord(move[2]);
+    const newX: number = Number(move[3]) - 1;
+
+  }
+  public getBestMove(fen: string): Observable<ChessMove> {
+    const queryParams: StockfishQueryParams = {
+      fen,
+      depth : 13,
+      mode: 'bestmove'
+    };
+
+    let params = new HttpParams().appendAll(queryParams);
+
+    return this.http.get<StockFishResponse>(this.api, {params})
+    .pipe(
+      switchMap(response => {
+        const bestMove: string =  response.data.split(' ')[1];
+      })
+    )
+  }
 }

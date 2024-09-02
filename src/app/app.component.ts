7cbf642d89640b044bd4e7d4d1b820a20703eadf
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-02 19:08:45 +0300

    add: HttpClientModule to AppComponent imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChessBoardComponent } from './modules/chess-board/chess-board.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChessBoardComponent],
+  imports: [RouterOutlet, ChessBoardComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

1e345d1568ac55581b676ac25d25b7920492b7a4
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-03 20:02:35 +0300

    add: MatToolbarModule, MataButtonModule and RouterModule to nav-menu.component

--- src/app/modules/nav-menu/nav-menu.component.ts
+++ src/app/modules/nav-menu/nav-menu.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, RouterModule],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.css'
 })

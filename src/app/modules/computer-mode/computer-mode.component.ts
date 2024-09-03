2342a2d29c65dc690378a49c4126ca4e8dd36a17
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-03 20:04:52 +0300

    add: CommonModule import in computer-mode.component.ts

--- src/app/modules/computer-mode/computer-mode.component.ts
+++ src/app/modules/computer-mode/computer-mode.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ChessBoardComponent } from '../chess-board/chess-board.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-computer-mode',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: '../chess-board/chess-board.component.html',
   styleUrl: '../chess-board/chess-board.component.css'
 })

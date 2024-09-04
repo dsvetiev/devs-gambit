19ab181cc2d7d88b3ba526f1a0ccf6009ed0e9d8
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-04 20:36:18 +0300

    add: initial implementation of ComputerModeComponent

--- src/app/modules/computer-mode/computer-mode.component.ts
+++ src/app/modules/computer-mode/computer-mode.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChessBoardComponent } from '../chess-board/chess-board.component';
 import { CommonModule } from '@angular/common';
+import { StockfishService } from './stockfish.service';
+import { ChessBoardService } from '../chess-board/chess-board.service';
 
 @Component({
   selector: 'app-computer-mode',
@@ -9,6 +11,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: '../chess-board/chess-board.component.html',
   styleUrl: '../chess-board/chess-board.component.css'
 })
-export class ComputerModeComponent extends ChessBoardComponent {
+export class ComputerModeComponent extends ChessBoardComponent implements OnInit {
 
+  constructor(private stockFishService: StockfishService,
+    ChessBoardService: ChessBoardService
+  ) {
+    super(ChessBoardService);
+  }
+  ngOnInit(): void {
+    
+  }
 }

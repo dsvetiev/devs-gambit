a0ff8e6cb057ff54681badaff0d124ff92b14f98
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-06 19:05:18 +0300

    add: implement play method and constructor in PlayAgainstComputerDialogComponent

--- src/app/modules/play-against-computer-dialog/play-against-computer-dialog.component.ts
+++ src/app/modules/play-against-computer-dialog/play-against-computer-dialog.component.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { StockfishService } from '../computer-mode/stockfish.service';
+import { Color } from '../../chess-logic/models';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-play-against-computer-dialog',
@@ -14,7 +17,22 @@ export class PlayAgainstComputerDialogComponent {
     public stockfishLevels: readonly number[] = [1, 2, 3, 4, 5];
     public stockfishLevel: number = 1;
 
+    constructor(
+      private stockfishService: StockfishService,
+      private dialog: MatDialog,
+      private router: Router
+    ) { }
+     
     public selectStockfishLevel(level: number): void {
       this.stockfishLevel = level;
+    }
+
+    public play(color: 'w' | 'b'): void {
+      this.dialog.closeAll();
+      this.stockfishService.computerConfiguration$.next({
+        color: color === 'w' ? Color.White : Color.Black,
+        level: this.stockfishLevel
+      });
+      this.router.navigate(['against-computer']);
     }
 }

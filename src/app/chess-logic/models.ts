1be30426097c182fd24312ad3a8249296d045ea2
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-07 19:11:17 +0300

    add: MoveType enum containing the chess game move types and moveType property in LastMove type

--- src/app/chess-logic/models.ts
+++ src/app/chess-logic/models.ts
@@ -42,12 +42,22 @@ export const pieceImagePaths: Readonly<Record<FENChar, string>> = {
 
 export type SafeSquares = Map<string, Coords[]>;
 
+export enum MoveType {
+    Capture,
+    Castling,
+    Promotion,
+    Check,
+    CheckMate,
+    BasicMove
+}
+
 export type LastMove = {
     piece: Piece;
     prevX: number;
     prevY: number;
     currX: number;
     currY: number;
+    moveType: Set<MoveType>;
 }
 
 type KingChecked = {

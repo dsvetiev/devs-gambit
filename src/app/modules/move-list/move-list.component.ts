911844a76c7804a868cbca7665107f27cd9a9943
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-09 16:34:05 +0300

    add: generate move-list component

---
+++ src/app/modules/move-list/move-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-move-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './move-list.component.html',
+  styleUrl: './move-list.component.css'
+})
+export class MoveListComponent {
+
+}

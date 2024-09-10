bca4caf54ee22d3694f27e42f706bee335963c40
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-10 19:38:18 +0300

    add: update AppComponent imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,18 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { NavMenuComponent } from './modules/nav-menu/nav-menu.component';
 import { PlayAgainstComputerDialogComponent } from './modules/play-against-computer-dialog/play-against-computer-dialog.component';
+import { MoveListComponent } from './modules/move-list/move-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChessBoardComponent, HttpClientModule, NavMenuComponent, PlayAgainstComputerDialogComponent],
+  imports: [
+    RouterOutlet,
+    ChessBoardComponent,
+    HttpClientModule,
+    NavMenuComponent,
+    PlayAgainstComputerDialogComponent,
+    MoveListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

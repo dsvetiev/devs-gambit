7f5d304b7733a0af246d7f93f34c75f22872d1c3
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-10 19:42:28 +0300

    add: required @Input properties to MoveListComponent

--- src/app/modules/move-list/move-list.component.ts
+++ src/app/modules/move-list/move-list.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MoveList } from '../../chess-logic/models';
+import { ChessBoardComponent } from '../chess-board/chess-board.component';
 
 @Component({
   selector: 'app-move-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatIconModule, ChessBoardComponent],
   templateUrl: './move-list.component.html',
   styleUrl: './move-list.component.css'
 })
 export class MoveListComponent {
-
+@Input({ required: true }) public moveList!: MoveList;
+@Input({ required: true }) public gameHistoryPointer: number = 0;
+@Input({ required: true}) public gameHistoryLength: number = 1;
 }

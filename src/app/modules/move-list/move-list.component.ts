1be2f6f338eef52c6e38a153e1547f71d76d322f
DilyanSvetiev <dsvetiev@gmail.com>
2024-09-10 19:46:07 +0300

    add: @Output showPreviousPositionEvent and showPreviousPosition method to MoveListComponent

--- src/app/modules/move-list/move-list.component.ts
+++ src/app/modules/move-list/move-list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MoveList } from '../../chess-logic/models';
@@ -16,4 +16,9 @@ export class MoveListComponent {
 @Input({ required: true }) public moveList!: MoveList;
 @Input({ required: true }) public gameHistoryPointer: number = 0;
 @Input({ required: true}) public gameHistoryLength: number = 1;
+@Output() public showPreviousPositionEvent = new EventEmitter<number>();
+
+public showPreviousPosition(moveIndex: number): void {
+  this.showPreviousPositionEvent.emit(moveIndex);
+}
 }
